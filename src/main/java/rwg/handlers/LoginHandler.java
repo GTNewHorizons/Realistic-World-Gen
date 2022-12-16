ae5563f588a7652a10d79a1133a3024816df4eac
David Lindström <info@davidlindstrom.se>
2022-12-16 18:40:02 +0100

    Remove the warning on login as we currently can't detect new worlds

--- src/main/java/rwg/handlers/LoginHandler.java
+++ src/main/java/rwg/handlers/LoginHandler.java
@@ -1,23 +1,18 @@
 package rwg.handlers;
 
-import static net.minecraft.util.EnumChatFormatting.GOLD;
-import static net.minecraft.util.EnumChatFormatting.RED;
-import static net.minecraft.util.EnumChatFormatting.WHITE;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
-import net.minecraft.util.ChatComponentText;
-import rwg.util.NoiseImplementation;
-import rwg.world.RwgWorldSavedData;
 
 public class LoginHandler {
     @SuppressWarnings("unused")
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        if (RwgWorldSavedData.getNoiseImplementation() == NoiseImplementation.UNKNOWN) {
-            event.player.addChatMessage(new ChatComponentText(RED
-                    + "WARNING: Unable to detect worldgen version for this world. This can cause chunk border mismatch if not corrected. Please run "
-                    + GOLD + "/rwg_buginfo" + WHITE + " for info on how to fix."));
-        }
+
+        //        if (RwgWorldSavedData.getNoiseImplementation() == NoiseImplementation.UNKNOWN) {
+        //            event.player.addChatMessage(new ChatComponentText(RED
+        //                    + "WARNING: Unable to detect worldgen version for this world. This can cause chunk border
+        // mismatch if not corrected. Please run "
+        //                    + GOLD + "/rwg_buginfo" + WHITE + " for info on how to fix."));
+        //        }
     }
 }
